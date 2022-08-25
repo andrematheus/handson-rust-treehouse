03b04bb6d57e79a8a6dbd647b4638244522027e2
André Roque Matheus <amatheus@ligandoospontos.com.br>
2022-08-25 17:39:40 -0300

    Loop for visitors and add unknown to list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::stdin;
 
+#[derive(Debug)]
 struct Visitor {
     name: String,
     greeting: String,
@@ -29,20 +30,31 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    println!("Hello, what's your name?");
-    let mut name = what_is_your_name();
-
-    let visitor_list = vec![
+    let mut visitor_list = vec![
         Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
         Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
         Visitor::new("fred", "Wow, who invited Fred?"),
     ];
 
-    let known_visitor = visitor_list
-        .iter()
-        .find(|visitor| visitor.name == name);
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        None => println!("Your are not in the visitor list. Please leave."),
+    loop {
+        println!("Hello, what's your name? (Leave empty and press ENTER to quit)");
+        let mut name = what_is_your_name();
+
+        let known_visitor = visitor_list
+            .iter()
+            .find(|visitor| visitor.name == name);
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, "New friend"));
+                }
+            }
+        }
     }
+    println!("The final list of visitors:");
+    println!("{:#?}", visitor_list);
 }
